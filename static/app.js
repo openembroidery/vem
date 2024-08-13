4f9c261c1a9fa39399b59e4799c0c660b544d70e
Fathony L <fathony@smart-leaders.net>
2024-08-13 13:23:42 +0700

    draggable plotter

--- static/app.js
+++ static/app.js
@@ -1,31 +1,59 @@
 const TEMPLATES = await (await fetch('static/app.xml')).text();
-// This example illustrates how one can write Owl components with
-// inline templates.
-
-import { Component, useState, xml, mount } from "@odoo/owl";
+import { Component, useState, useRef, mount } from "@odoo/owl";
 
 // Counter component
 class Vem extends Component {
-  static template = 'Vem';
+    static template = 'Vem';
 
-  setup(){
-    this.state = useState({ 
-      x:0, y:0, 
-      direction:null, value: 0 
-    })
-  }
+    setup(){
+        this.state = useState({ 
+            x:0, y:0, 
+            direction:null, value: 0 
+        })
+        this.plotterRef = useRef('plotter')
+    }
 
-  navClick(ev){
-    const go = ev.target.getAttribute('go')
-    const [direction,value] = this._goToDir(go);
-    this.state[direction] += value;
-    console.log(go, direction, value)
-  }
-  _goToDir(go){
-    const actions = [['y',-1],['x',1],['y',1],['x',-1]]
-    go = ['up', 'right', 'down', 'left'].indexOf(go)
-    return actions[go]
-  }
+    navClick(ev){
+        const go = ev.target.getAttribute('go')
+        const [direction,value] = this._goToDir(go);
+        this.state[direction] += value;
+        console.log(go, direction, value)
+    }
+    _goToDir(go){
+        const actions = [['y',-1],['x',1],['y',1],['x',-1]]
+        go = ['up', 'right', 'down', 'left'].indexOf(go)
+        return actions[go]
+    }
+
+    plotterBeginDrag(ev){
+        console.log('begin drag')
+        const el = this.plotterRef.el;
+        const offsetX = ev.pageX;
+        const offsetY =  ev.pageY;
+        const x = this.state.x, y = this.state.y;
+        let dx, dy;
+
+        
+        const moveWindow = (ev) => {
+            dx = ev.pageX - offsetX;
+            dy = ev.pageY - offsetY;
+            // el.style.setProperty('--x', `${x+dx}`);
+            // el.style.setProperty('--y', `${y+dy}`);
+            this.state.x = x + dx;
+            this.state.y = y + dy;
+        }
+        const stopDnD = () => {
+            window.removeEventListener("mousemove", moveWindow);
+            el.classList.remove('dragging');
+            
+            if (dy !== undefined && dx !== undefined) {
+                // self.windowService.updatePosition(current.id, dx, dy);
+            }
+        }
+
+        window.addEventListener("mousemove", moveWindow);
+        window.addEventListener("mouseup", stopDnD, { once: true });
+    }
 }
 
 // Application setup
